b5e41401e800d1f1053003681666078e74a60723
Lyceris-chan <ghostdrain@outlook.com>
2020-12-12 22:15:25 +0000

    daisy: Boardconfig: set TARGET_HAS_BROKEN_WLAN_SET_INTERFACE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -243,6 +243,7 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_USES_AOSP_WLAN_HAL := true
+TARGET_HAS_BROKEN_WLAN_SET_INTERFACE := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/daisy/BoardConfigVendor.mk
